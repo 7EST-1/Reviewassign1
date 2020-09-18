c1c4c6467148822a2ea778719781a5a89ceec7eb
cart8810 <cart8810@ISS2V3QKH2.scdsb.pri>
2020-09-18 09:39:13 -0400

    updated

--- src/part1/BarChart.java
+++ src/part1/BarChart.java
@@ -1,13 +1,53 @@
 package part1;
 
 import TurtleGraphics.*;
-        
+import BreezySwing.*;
+import java.awt.Color;
+import java.util.Scanner;
+
 public class BarChart {
 
-    
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        SketchPadWindow spw = new SketchPadWindow(800, 600);
+        Pen p = new StandardPen(spw);
+
+        int bar1, bar2, bar3, bar4;
+        System.out.println("Enter width for bar 1: >");
+        bar1 = s.nextInt();
+        System.out.println("Enter width for bar 2: >");
+        bar2 = s.nextInt();
+        System.out.println("Enter width for bar 3: >");
+        bar3 = s.nextInt();
+        System.out.println("Enter width for bar 4: >");
+        bar4 = s.nextInt();
         
         
+       
+        makeBar(p, -200, -250, bar2, Color.red);
+        makeBar(p, -100, -250, bar1, Color.blue);
+        makeBar(p, 0, -250, bar3, Color.green);
+        makeBar(p, 100, -250, bar4, Color.yellow);
+        p.up();
+        p.move(400,-250);
+        p.setColor(Color.black);
+        p.down();
+        p.move(-400,-250);
+
     }
-    
+
+    public static void makeBar(Pen p, int xloc, int yloc, int length, Color col) {
+        p.up();
+        p.move(xloc, yloc);
+        p.down();
+        p.setWidth(10);
+        p.setColor(col);
+        p.setDirection(90);
+        p.move(length);
+        p.up();
+        p.move(15);
+        p.down();
+        p.drawString(length + "");
+    }
+
 }
--- src/part1/PizzaCost1.java
+++ src/part1/PizzaCost1.java
@@ -1,10 +1,32 @@
 package part1;
 
+import java.util.Scanner;
+
 
 public class PizzaCost1 {
 
     
     public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        final double labor = 1.5;
+        final double  heat = 0.75;
+        double size;
+        int nump;
+        System.out.println(" Enter your pizza in inches: >");
+        
+        size = s.nextDouble();
+        
+        System.out.println("Enter the amount of topping for your pizza: >");
+        
+        nump = s.nextInt();
+        
+        //calc
+        double total;
+        total = labor + heat + size * .50 + nump * 0.75;
+                System.out.println("Your pizza will cost: >" +total);       
+        
+        
+        
        
     }
     
